837df79e6a18df20e06280178b1396601654a152
Arthurdosanjos <Arthur.barros2009@hotmail.com>
2020-03-21 20:30:57 -0300

    Imagens e Urls

--- controllers/foodCard.js
+++ controllers/foodCard.js
@@ -1,11 +1,11 @@
 const Food = require("../models/Food");
 
 const foodArray = [
-  new Food("French Fries", "R$ 6.00", "312 cal"),
-  new Food("Abbacchio", "R$ 44.99", "444 cal"),
-  new Food("Caesar Salad", "R$ 7.95", "44 cal"),
-  new Food("Panna Cotta", "R$ 25.00", "708 cal"),
-  new Food("Macchiato", "R$ 5.00", "140 cal"),
+  new Food("French Fries", "R$ 6.00", "312 kcal", "imagens/French-Fries.jpg"),
+  new Food("Abbacchio", "R$ 44.99", "444 kcal", "imagens/Abbacchio.jpg"),
+  new Food("Caesar Salad", "R$ 7.95", "44 kcal", "imagens/Caesar-Salad.jpg"),
+  new Food("Panna Cotta", "R$ 25.00", "708 kcal", "imagens/Panna-Cotta.jpg"),
+  new Food("Macchiato", "R$ 5.00", "140 kcal", "imagens/Macchiato.jpg"),
 ];
 
 exports.renderFoodCard = (require, response) => {
