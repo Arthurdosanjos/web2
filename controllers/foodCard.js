f609f676372a723c473a11f64a0cf34e64bf4859
arthurdosanjos <arthur.barros2009@hotmail.com>
2020-03-28 23:18:26 -0300

    abbacchio atualizado

--- controllers/foodCard.js
+++ controllers/foodCard.js
@@ -3,7 +3,7 @@ const Food = require("../models/Food");
 const foodArray = [
 Food("Macchiato", "R$ 5.00", "140 cal","frenchfries.jpeg"),
   new Food("French Fries", "R$ 6.00", "312 kcal", "imagens/French-Fries.jpg"),
-  new Food("Abbacchio", "R$ 44.99", "444 kcal", "imagens/Abbacchio.jpg"),
+  new Food("Abbacchio", "R$ 44.99", "444 kcal", "imagens/Abbacchio.png"),
   new Food("Caesar Salad", "R$ 7.95", "44 kcal", "imagens/Caesar-Salad.jpg"),
   new Food("Panna Cotta", "R$ 25.00", "708 kcal", "imagens/Panna-Cotta.jpg"),
   new Food("Macchiato", "R$ 5.00", "140 kcal", "imagens/Macchiato.jpg"),
