4687089ea5ff8d62f81a719100c0ddcbaa3b1e61
Marcos Antônio <marcos.antonioluz36@gmail.com>
2020-03-28 21:30:48 -0300

    Cortei duas letras de food na hora de arrumar o merge, foi mal.

--- controllers/foodCard.js
+++ controllers/foodCard.js
@@ -1,7 +1,7 @@
 const Food = require("../models/Food");
 
 const foodArray = [
-od("Macchiato", "R$ 5.00", "140 cal","frenchfries.jpeg"),
+Food("Macchiato", "R$ 5.00", "140 cal","frenchfries.jpeg"),
   new Food("French Fries", "R$ 6.00", "312 kcal", "imagens/French-Fries.jpg"),
   new Food("Abbacchio", "R$ 44.99", "444 kcal", "imagens/Abbacchio.jpg"),
   new Food("Caesar Salad", "R$ 7.95", "44 kcal", "imagens/Caesar-Salad.jpg"),
